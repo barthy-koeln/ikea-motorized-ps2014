43548b5e15eeba6a6ef1b75425456be807379504
barthy <b.bonhomme@vucx.de>
2022-09-18 23:15:54 +0200

    [FEATURE] prepare IR receiver

--- servooo.ino
+++ servooo.ino
@@ -1,18 +1,40 @@
+#define DECODE_NEC
+#include <IRremote.hpp>
 
-#define MOTOR_A_IN1 5
-#define MOTOR_A_IN2 6
+#define MOTOR_A_IN1 2
+#define MOTOR_A_IN2 3
 
 #define ROTARY_DT 7
 #define ROTARY_CLK 8
 
+#define IR_RECEIVE 9
+
 #define ROTARY_MAX_STOP 24
+
+#define BUTTON_1 0x45
+#define BUTTON_2 0x46
+#define BUTTON_3 0x47
+#define BUTTON_4 0x44
+#define BUTTON_5 0x40
+#define BUTTON_6 0x43
+#define BUTTON_7 0x7
+#define BUTTON_8 0x15
+#define BUTTON_9 0x9
+#define BUTTON_ASTERISK 0x16
+#define BUTTON_0 0x19
+#define BUTTON_POUND 0xD
+#define BUTTON_LEFT 0x8
+#define BUTTON_UP 0x18
+#define BUTTON_RIGHT 0x5A
+#define BUTTON_DOWN 0x52
+#define BUTTON_OK 0x1C
+
 
 uint8_t rotaryCounter = 0;
 int currentStateCLK;
 int lastStateCLK;
 
-void setup()
-{
+void setup() {
   Serial.begin(9600);
 
   pinMode(MOTOR_A_IN1, OUTPUT);
@@ -20,6 +42,10 @@ void setup()
 
   pinMode(ROTARY_CLK, INPUT);
   pinMode(ROTARY_DT, INPUT);
+
+  pinMode(ROTARY_DT, INPUT);
+
+  IrReceiver.begin(IR_RECEIVE, true);
 
   lastStateCLK = digitalRead(ROTARY_CLK);
 }
@@ -78,10 +104,30 @@ void motorStop(uint16_t duration) {
   }
 }
 
-void loop()
-{
-  motorRollUp(3);
-  motorStop(1000);
-  motorRollDown(3);
-  motorStop(1000);
+void loop() {
+
+  //  digitalWrite(MOTOR_A_IN1, HIGH);
+  //  digitalWrite(MOTOR_A_IN2, LOW);
+  //  delay(300);
+  //
+  //  digitalWrite(MOTOR_A_IN1, LOW);
+  //  digitalWrite(MOTOR_A_IN2, HIGH);
+  //  delay(300);
+  //
+  //  digitalWrite(MOTOR_A_IN1, LOW);
+  //  digitalWrite(MOTOR_A_IN2, LOW);
+  //  delay(300);
+
+
+  if (IrReceiver.decode()) {
+    Serial.println(IrReceiver.decodedIRData.decodedRawData, HEX);
+    IrReceiver.printIRResultShort(&Serial);
+
+    IrReceiver.resume();
+  }
+
+  //  motorRollUp(3);
+  //  motorStop(1000);
+  //  motorRollDown(3);
+  //  motorStop(1000);
 }
